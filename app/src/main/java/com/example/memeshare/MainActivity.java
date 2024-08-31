c3fa7ae74c0898015567e3e92a85623f1d820175
YASH <pyash7267@gmail.com>
2024-08-31 15:38:41 +0530

    DONE

---
+++ app/src/main/java/com/example/memeshare/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.memeshare;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.DrawableWrapper;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static android.content.ContentValues.TAG;
+
+public class MainActivity extends AppCompatActivity {
+    String currentImageUrl;
+    ImageView imageView;
+    ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadMeme();
+    }
+
+    public void loadMeme(){
+        progressBar= findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+
+
+        String url ="https://meme-api.herokuapp.com/gimme";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                   String url = response.getString("url");
+                   currentImageUrl =url;
+                    imageView = findViewById(R.id.memeImageView);
+                    Glide.with(MainActivity.this).load(url).listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable @org.jetbrains.annotations.Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            progressBar.setVisibility(View.GONE);
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            progressBar.setVisibility(View.GONE);
+                            return false;
+                        }
+                    }).into(imageView);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Something went Wrong", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+
+    }
+
+    public void shareMeme(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,"Hey, Checkout This Cool Meme I Got from Reddit : "+currentImageUrl);
+        Intent chooser= Intent.createChooser(intent,"Share this meme using...");
+        startActivity(chooser);
+    }
+
+    public void nextMeme(View view) {
+        loadMeme();
+    }
+}
